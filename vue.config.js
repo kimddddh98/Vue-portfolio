e10173bcac18679eb8c80e179f76c627360c2517
kimddddh98 <104114436+kimddddh98@users.noreply.github.com>
2022-08-30 17:03:01 +0900

    change output directory

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//       ? '/docs'
+//       : '/'
+//   }
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/docs'
-      : '/'
+    ? '/githubPageTest/'
+    : '/',
+   outputDir: 'docs'
   }
